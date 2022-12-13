c31b97080cada3fe48b289ad8b75a5d6a74746ae
진용현 <yonghyuj@Macbook-Air.local>
2022-12-12 20:16:19 -0800

    fake database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import programdatabase from './FakeDatabase';
 import './App.css';
+import { HiChevronLeft,HiMagnifyingGlass } from "react-icons/hi2";
+import Accordion from 'react-bootstrap/Accordion'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.css'
+import store from './redux/store'
+
+
 
 function App() {
+  const [fakeData, setFakeData] = useState(store.getState())
+  const [completion,changeCompletion]= useState(20)
+
+  useEffect(() => {
+    //Runs on the first render
+    //And any time any dependency value changes
+  
+  }, fakeData);
+
+  function updateProgress(){
+    if()
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="program">
+        <div className="programHeader">
+          <div className="program_title">
+              Michelle'sProgram
+          </div>
+          <div className="program_progress">
+            <div className="direct">
+              <div className="direct_icon">
+                <HiChevronLeft/>
+              </div>
+              <div className="direct_text">
+                Go to Dashboard
+              </div>
+            </div>
+            <div className="displayProgress">
+              <div className="progress_title">
+                Your First Week
+              </div>
+              <ProgressBar now={completion}/>
+              <div className="progress_completion">
+                  <strong>{completion}</strong>% complete
+              </div>
+               
+              
+            </div>
+          </div>
+          <div className="searchBox">
+          <InputGroup className="searchBox_input">
+            <InputGroup.Text><HiMagnifyingGlass/></InputGroup.Text>
+            <Form.Control placeholder="Search by assignment"/>
+          </InputGroup>
+          </div>
+        </div>
+        <div className='programList'>
+          
+            {programdatabase.map((route,key) => (
+            <Accordion className="list">
+              <Accordion.Item>
+              <Accordion.Header>{route.name}</Accordion.Header>
+                <Accordion.Body>
+                  {route.subSteps.map((item,key) =>(
+                    <div className="task">
+                      <div className="task_main">
+                        <div className="completion">
+                          <Form.Check id={key} defaultChecked={item.finished}/>
+                        </div>
+                        <div className="title">{item.name}</div>
+                      </div>
+                      <div className="task_description">
+                        <div className="icon_des">
+                          {item.icon}
+                        </div>
+                        <div className="type_des">
+                          {item.style}
+                        </div>
+                        <div className="duration_des">
+                          {item.time}
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                  }
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            
+            ))
+          }
+        
+        </div>
+        
     </div>
   );
 }
 
 export default App;
+
+
+// {programdatabase.map((route,key) => (
+//   <Accordion 
+//     title={route.name} 
+//     children={
+//       route.subSteps.map((item,key) =>(
+//         <div>{item.name}</div>
+//       ))
+//     }/>
+  
+// ))
+// }
---
+++ src/FakeDatabase.js
@@ -0,0 +1,134 @@
+import { HiOutlineDocumentText, HiOutlineVideoCamera } from 'react-icons/hi2';
+
+const ProgramDatabase = [
+    {
+     name: "Week 1",
+     number: 4,
+     subSteps:[
+        {
+        name: "Watch video on personal finance ",
+        icon:<HiOutlineVideoCamera/>,
+        style:"VIDEO",
+        time:"2 MIN",
+        finished: true,
+     },{
+        name: "Create savings goal",
+        icon:<HiOutlineDocumentText/>,
+        style:"TEXT",
+        finished: true,
+     },{
+        name: "Automate savings",
+        icon:<HiOutlineDocumentText/>,
+        style:"TEXT",
+        finished: false,
+     },{
+        name: "Vacation fund",
+        icon:<HiOutlineDocumentText/>,
+        style:"TEXT",
+        finished: false,
+     },
+     {
+        name: "Vacation fund",
+        icon:<HiOutlineDocumentText/>,
+        style:"TEXT",
+        finished: false,
+     }
+     ],
+     
+    },
+    {
+        name: "Week 2",
+        number: 3,
+        subSteps:[
+           {
+           name: "Watch video on personal finance 2",
+           icon:<HiOutlineVideoCamera/>,
+           style:"VIDEO",
+           time:'2 MIN'
+        },{
+           name: "Create savings goa2l",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        },{
+           name: "Automate savings2",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        }
+        ],
+    },
+    {
+        name: "Week 3",
+        number: 4,
+        subSteps:[
+           {
+           name: "Watch video on personal finance3 ",
+           icon:<HiOutlineVideoCamera/>,
+           style:"VIDEO",
+           time:'2 MIN'
+        },{
+           name: "Create savings goal3",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        },{
+           name: "Automate savings3",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        },{
+           name: "Vacation fund3",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        }
+        ],
+    },
+    {
+        name: "Week 4",
+        number: 4,
+        subSteps:[
+           {
+           name: "Watch video on personal finance4 ",
+           icon:<HiOutlineVideoCamera/>,
+           style:"VIDEO",
+           time:'2 MIN'
+        },{
+           name: "Create savings goal4",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        },{
+           name: "Automate savings4",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        },{
+           name: "Vacation fund4",
+           icon:<HiOutlineDocumentText/>,
+           style:"TEXT",
+        }
+        ],
+    },
+    {
+        name: "Week 5",
+        number: 4,
+        subSteps:[
+            {
+            name: "Watch video on personal finance5 ",
+            icon:<HiOutlineVideoCamera/>,
+            time:'2 MIN',
+            style:"VIDEO",
+         },{
+            name: "Create savings goal5",
+            icon:<HiOutlineDocumentText/>,
+            style:"TEXT",
+         },{
+            name: "Automate savings5",
+            icon:<HiOutlineDocumentText/>,
+            style:"TEXT",
+         },{
+            name: "Vacation fund5",
+            icon:<HiOutlineDocumentText/>,
+            style:"TEXT",
+         }
+         ],
+    },
+   
+  ];
+  
+  export default ProgramDatabase
